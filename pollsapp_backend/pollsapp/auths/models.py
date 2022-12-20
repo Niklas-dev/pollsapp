a2deb8cccd476f784b45db04d8d2feb88bfa473b
Niklas-dev <business-mail-niklas@web.de>
2022-12-20 23:17:31 +0100

    Adding migrations

--- pollsapp_backend/pollsapp/auths/models.py
+++ pollsapp_backend/pollsapp/auths/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from polls.models import Poll
 
 # Create your models here.
+
--- pollsapp_backend/pollsapp/polls/models.py
+++ pollsapp_backend/pollsapp/polls/models.py
@@ -1,7 +1,13 @@
-from django.db import models
+from django.db import models 
 
 # Create your models here.
 
 
-class Polls(models.Model):
-    pass
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    published_date = models.DateField(auto_now_add=True)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
